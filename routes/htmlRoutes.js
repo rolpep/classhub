95c85c9380929fb32e13d7a0c2613540bb532224
“dylanburkey” <dylanburkey@outlook.com>
2018-08-18 13:31:25 -0400

    add updated partials and styles

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -20,6 +20,16 @@ module.exports = function(app) {
     });
   });
 
+    // Load example page and pass in an example by id
+    app.get("/login/:id", function(req, res) {
+      db.dbSignUp.findOne({ where: { id: req.params.id } }).then(function(dbSignUp) {
+        res.render("login", {
+          example: dbSignUp
+        });
+      });
+    });
+  
+
   // Render 404 page for any unmatched routes
   app.get("*", function(req, res) {
     res.render("404");
